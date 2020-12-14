b3cc6577286e8f154565912347d8d2bd584d1b32
Ryo Miyajima <sergeant.wizard@gmail.com>
2020-12-14 22:22:48 +0900

    add constexpr pin values

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,39 @@
 
 using namespace ubiknob;
 
+// single mode pin
+static constexpr int PIN_SM1 = 0;
+static constexpr int PIN_SM2 = 1;
+// dual mode pin
+static constexpr int PIN_DM1 = 2;
+static constexpr int PIN_DM2 = 3;
+
+// single value pin
+static constexpr int PIN_SV1 = 4;
+static constexpr int PIN_SV2 = 5;
+// dual value inner pin
+static constexpr int PIN_DVI1 = 6;
+static constexpr int PIN_DVI2 = 7;
+// dual value outer pin
+static constexpr int PIN_DVO1 = 8;
+static constexpr int PIN_DVO2 = 9;
+
+// buttons
+static constexpr int PIN_SB = 10;
+static constexpr int PIN_DB = 11;
+
 // modes
-static auto single_mode_knob = KnobReader(0, 0);
+static auto single_mode_knob = KnobReader(PIN_SM1, PIN_SM2);
 static auto single_mode_selector = ModeSelector<SingleKnobMode, NUM_SINGLE_KNOB_MODE>(SingleKnobMode::mode_alt);
-static auto dual_mode_knob = KnobReader(0, 0);
+static auto dual_mode_knob = KnobReader(PIN_DM1, PIN_DM2);
 static auto dual_mode_selector = ModeSelector<DualKnobMode, NUM_DUAL_KNOB_MODE>(DualKnobMode::mode_com1);
 
 // values
-static auto single_value_knob = KnobReader(0, 0);
-static auto single_value_button = ButtonReader(0);
-static auto dual_value_inner_knob = KnobReader(0, 0);
-static auto dual_value_outer_knob = KnobReader(0, 0);
-static auto dual_value_button = ButtonReader(0);
+static auto single_value_knob = KnobReader(PIN_SV1, PIN_SV2);
+static auto single_value_button = ButtonReader(PIN_SB);
+static auto dual_value_inner_knob = KnobReader(PIN_DVI1, PIN_DVI2);
+static auto dual_value_outer_knob = KnobReader(PIN_DVO1, PIN_DVO2);
+static auto dual_value_button = ButtonReader(PIN_DB);
 
 // TODO: set pins
 static auto lcd = ubiknob::LCD(0, 0, 0, 0, 0, 0);
