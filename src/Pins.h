b461823e6efab35120791fef045a8861e80a9262
Ryo Miyajima <sergeant.wizard@gmail.com>
2020-12-30 23:27:10 +0900

    use pins file

---
+++ src/Pins.h
@@ -0,0 +1,29 @@
+#pragma once
+
+namespace ubiknob {
+    static constexpr int PIN_RIGHT_BUTTON = 7;
+    static constexpr int PIN_RIGHT_INNER1 = 8;
+    static constexpr int PIN_RIGHT_INNER2 = 9;
+    static constexpr int PIN_RIGHT_OUTER1 = 11;
+    static constexpr int PIN_RIGHT_OUTER2 = 10;
+
+    static constexpr int PIN_MIDDLE_BUTTON = 12;
+    static constexpr int PIN_MIDDLE_INNER1 = 24;
+    static constexpr int PIN_MIDDLE_INNER2 = 25;
+    static constexpr int PIN_MIDDLE_OUTER1 = 27;
+    static constexpr int PIN_MIDDLE_OUTER2 = 26;
+
+    static constexpr int PIN_LEFT_BUTTON = 28;
+    static constexpr int PIN_LEFT_INNER1 = 29;
+    static constexpr int PIN_LEFT_INNER2 = 30;
+    static constexpr int PIN_LEFT_OUTER1 = 32;
+    static constexpr int PIN_LEFT_OUTER2 = 31;
+
+    // LCD
+    static constexpr int PIN_LCD_RS = 38;
+    static constexpr int PIN_LCD_EN = 37;
+    static constexpr int PIN_LCD_D4 = 36;
+    static constexpr int PIN_LCD_D5 = 35;
+    static constexpr int PIN_LCD_D6 = 34;
+    static constexpr int PIN_LCD_D7 = 33;
+}
